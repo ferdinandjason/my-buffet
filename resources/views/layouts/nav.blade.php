1a49499bac77f5f8275b88245c0fd0b95998ceb6
yolandahp <yolandahertita903@gmail.com>
2019-03-24 18:16:57 +0700

    gatahu ini apa

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -5,7 +5,7 @@
         @if( strpos(url()->current(), 'login') == false && strpos(url()->current(), 'register') == false )
             <div class="navbar-header">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <b>Express</b>Buffet
+                    <b>My</b>Buffet
                 </a>
 
                 <button type='button' class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -13,10 +13,27 @@
                 </button>
             </div>
 
-            <ul class="nav navbar-nav navbar-right collapse navbar-collapse">
-                <li><a href="{{ url('/login') }}">Login</a></li>
-                <li><a href="{{ url('/register') }}">Sign Up</a></li>
-            </ul>
+            @guest
+                <ul class="nav navbar-nav navbar-right collapse navbar-collapse">
+                    <li><a href="{{ url('/login') }}">Sign In</a></li>
+                    <li><a href="{{ url('/register') }}">Sign Up</a></li>
+                </ul>
+            @else
+                <ul class="nav navbar-nav navbar-right collapse navbar-collapse">
+                    <li class="dropdown">
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        {{ Auth::user()->nama }} <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">
+                            <span class="glyphicon glyphicon-user" aria-hidden="true"></span>My Profile
+                            </a></li>
+                            <li><a href="{{ url(config('adminlte.logout_url', 'auth/logout')) }}">
+                                <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>Log Out
+                            </a></li>
+                        </ul>
+                    </li>
+                </ul>
+            @endguest
         @else
             <div class="navbar-header">
                 <a class="navbar-brand" href="{{ url('/') }}">
--- resources/views/users/order.blade.php
+++ resources/views/users/order.blade.php
@@ -1,46 +1,83 @@
-<script src="http://localhost:8000/vendor/adminlte/vendor/jquery/dist/jquery.min.js"></script>
+@extends('adminlte::master')
 
-<h1><a href="{{ url(config('adminlte.logout_url', 'auth/logout')) }}">
-    <i class="fa fa-fw fa-power-off"></i> {{ trans('adminlte::adminlte.log_out') }}
-</a></h1>
+@section('adminlte_css')
+    <style>
+        .card {
+            background-color: #fff;
+            border: 1px solid transparent; 
+            border-radius: 6px; 
+        }
 
-@foreach ($restaurants as $resto)
-    <p>{{$resto->id}}</p>
-    <p>{{$resto->nama}}</p>
-    <p>{{$resto->alamat}}</p>
-    <p>{{$resto->nomor_telepon}}</p>
-    <button onclick="pilihRestaurant({{$resto->id}})">Pilih Resto +</button>
-    <p> Menu nya adalah ---------------------------------------------- </p>
-    @if(is_null($resto->menu))
-        <p> No Menu Avaiable </p>
-    @else
-        @foreach ($resto->menu as $menu)
-            <img src="{{Storage::url($menu->foto)}}" style="height: 100px;width: 100px;"/>
-            <p>{{$menu->nama_makanan}}</p>
-            <p>{{$menu->deskripsi}}</p>
-            <p>{{$menu->harga}}</p>
-            <p>{{$menu->stok}}</p>
-            <button onclick="pilihMenu({{$menu->id}},'{{$menu->nama_makanan}}',{{$menu->harga}})">Tambah +</button>
-            <br/>
+        .card .card-caption {
+            position: absolute;
+            left: 30px;
+            bottom: 10px;
+            color: #fff;
+        }
+    </style>
+@stop
+
+@section('body')
+    @include('layouts.nav')
+    <div class="container">
+
+        @foreach ($restaurants as $resto)
+        
+        <div class="col-sm-4">
+            <div class="card">
+                <span class="card-img">
+                    <img src="https://fillmurray.com/g/640/320" class="img-responsive">
+                    <div class="card-caption">
+                        <span class="h2">{{$resto->nama}}</span>
+                    </div>
+                </span>
+            </div>
+        </div>
+
         @endforeach
-    @endif
-@endforeach
 
-<p> -------------------------------------------------------------- </p>
+        <!-- @foreach ($restaurants as $resto)
+            <p>{{$resto->id}}</p>
+            <p>{{$resto->nama}}</p>
+            <p>{{$resto->alamat}}</p>
+            <p>{{$resto->nomor_telepon}}</p>
+            <button onclick="pilihRestaurant({{$resto->id}})">Pilih Resto +</button>
+            <p> Menu nya adalah ---------------------------------------------- </p>
+            @if(is_null($resto->menu))
+                <p> No Menu Avaiable </p>
+            @else
+                @foreach ($resto->menu as $menu)
+                    <img src="{{Storage::url($menu->foto)}}" style="height: 100px;width: 100px;"/>
+                    <p>{{$menu->nama_makanan}}</p>
+                    <p>{{$menu->deskripsi}}</p>
+                    <p>{{$menu->harga}}</p>
+                    <p>{{$menu->stok}}</p>
+                    <button onclick="pilihMenu({{$menu->id}},'{{$menu->nama_makanan}}',{{$menu->harga}})">Tambah +</button>
+                    <br/>
+                @endforeach
+            @endif
+        @endforeach
 
-<form action={{route('user.order.final')}} method="POST" id="formOrder">
-    {{ csrf_field() }}
-    <input type="hidden" name="user_id" value="{{Auth::user()->id}}" id="user_id"/>
-    <input type="hidden" name="restaurant_id" value="0" id="restaurant_id"/>
-    <label for="comments">Note</label>
-    <input type="text" name="comments" id="comments"/>
-    <div id="dynamicField"></div>
-    <input name="total" id="total" value="0"/>
-    <button type="submit"> Beli </button>
-</form>
+        <p> -------------------------------------------------------------- </p>
+
+        <form action={{route('user.order.final')}} method="POST" id="formOrder">
+            {{ csrf_field() }}
+            <input type="hidden" name="user_id" value="{{Auth::user()->id}}" id="user_id"/>
+            <input type="hidden" name="restaurant_id" value="0" id="restaurant_id"/>
+            <label for="comments">Note</label>
+            <input type="text" name="comments" id="comments"/>
+            <div id="dynamicField"></div>
+            <input name="total" id="total" value="0"/>
+            <button type="submit"> Beli </button>
+        </form> -->
 
 
 
+
+    </div>
+@stop
+
+@section('adminlte_js')
 <script>
     $('#total').val(0)
     let orderMenuId = []
@@ -66,3 +103,4 @@
         }
     }
 </script>
+@stop
